eecba7f6f17e9448110ad7a35875d60d44a8c09e
sdcautomations <sdcautomations@gmail.com>
2025-03-06 22:03:35 +0100

    feat: add creating tasks

--- src/app.tsx
+++ src/app.tsx
@@ -4,9 +4,15 @@ import { Todo } from "./components";
 
 export const App = () => {
   const [tasks, setTasks] = useState<Array<string>>([]);
+  const [inputValue, setInputValue] = useState<string>("");
 
   const handleAddTask = (newTask: string) => {
     setTasks([...tasks, newTask]);
+    setInputValue("");
+  };
+
+  const handleInputChange = (value: any) => {
+    setInputValue(value);
   };
 
   return (
@@ -17,6 +23,27 @@ export const App = () => {
         {tasks.map((e: string) => {
           return <Todo content={e} />;
         })}
+      </div>
+
+      <div className={styles["app__form"]}>
+        <input
+          className={styles["app__form__input"]}
+          type="text"
+          value={inputValue}
+          placeholder="New task..."
+          onChange={(e: any) => {
+            handleInputChange(e.target.value);
+          }}
+        />
+
+        <div
+          className={styles["app__form__button"]}
+          onClick={() => {
+            handleAddTask(inputValue);
+          }}
+        >
+          Add Task
+        </div>
       </div>
     </main>
   );
--- src/components/todo/todo.tsx
+++ src/components/todo/todo.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./todo.module.scss";
 
 export type TodoProps = {
@@ -5,5 +6,18 @@ export type TodoProps = {
 };
 
 export const Todo = ({ content }: TodoProps) => {
-  return <div className={styles["todo"]}>{content}</div>;
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleClick = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <div
+      className={!isChecked ? styles["todo"] : styles["todo--checked"]}
+      onClick={handleClick}
+    >
+      {content}
+    </div>
+  );
 };
